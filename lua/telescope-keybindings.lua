bff4a18c795c527c93a0e9d247117db47d51bbac
Marcel <googlillo@gmail.com>
2021-03-21 13:03:38 +0100

    New telescope keybindings

--- lua/telescope-keybindings.lua
+++ lua/telescope-keybindings.lua
@@ -1,7 +1,7 @@
-vim.api.nvim_set_keymap('n', '<Leader>r', [[<Cmd> lua require('telescope.builtin').find_files()<cr>]], {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader>f', [[<Cmd> lua require('telescope.builtin').find_files()<cr>]], {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Leader>rg', [[<Cmd> lua require('telescope.builtin').grep_string({ search = vim.fn.input("Grep For > ")})<cr>]],
                         {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<Leader>rr', [[<Cmd> lua require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }<cr>]],
+vim.api.nvim_set_keymap('n', '<Leader>r', [[<Cmd> lua require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }<cr>]],
                         {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<Leader>rh', [[<Cmd> lua require('telescope.builtin').help_tags()<cr>]], {noremap = true, silent = true})
 
--- lua/tree-keybindings/init.lua
+++ lua/tree-keybindings/init.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<cr>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Leader><Tab>', ':NvimTreeToggle<cr>', {noremap = true, silent = true})
