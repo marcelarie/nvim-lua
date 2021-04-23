c7762bc225a445a359dd2c146c3b415eb55b56ee
Marcel Arie <googlillo@gmail.com>
2021-04-23 21:51:13 +0200

    add telescope default config

--- lua/nv-telescope/init.lua
+++ lua/nv-telescope/init.lua
@@ -10,22 +10,46 @@ ts.setup {
         file_previewer = require('telescope.previewers').vim_buffer_cat.new,
         grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
         qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-        file_ignore_patterns = {
-            "node_modules", "yarn.lock", "package-lock.json"
+
+        mappings = {i = {["<C-x>"] = false, ["<C-q>"] = actions.send_to_qflist}},
+        vimgrep_arguments = {
+            'rg', '--color=never', '--no-heading', '--with-filename',
+            '--line-number', '--column', '--smart-case'
         },
-        mappings = {i = {["<C-x>"] = false, ["<C-q>"] = actions.send_to_qflist}}
+        prompt_position = "bottom",
+        selection_caret = "> ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "descending",
+        layout_strategy = "horizontal",
+        layout_defaults = {
+            horizontal = {mirror = false},
+            vertical = {mirror = false}
+        },
+        file_ignore_patterns = {},
+        generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
+        shorten_path = true,
+        winblend = 0,
+        width = 0.75,
+        preview_cutoff = 120,
+        results_height = 1,
+        results_width = 0.8,
+        border = {},
+        borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+        use_less = true,
+        set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
+
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
     },
     extensions = {
         fzy_native = {
             override_generic_sorter = false,
             override_file_sorter = true
-        },
-        media_files = {}
+        }
     }
 }
 
 ts.load_extension('fzy_native')
-ts.load_extension('media_files')
-ts.load_extension('git_worktree')
-
 require'colorizer'.setup()
