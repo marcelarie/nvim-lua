d7425f96c5659befdc957cf86160dcb8c29880a8
Marcel Arie <googlillo@gmail.com>
2021-09-09 20:42:45 +0200

    move to plugins folder

--- init.lua
+++ init.lua
@@ -12,21 +12,21 @@ require("leader-remaps") -- ./lua/remaps.lua
 require("remaps") -- ./lua/remaps.lua
 
 -- /// TREE & EXPLORERS ///.
-require("undo-tree") -- ./lua/undo-tree/init.lua
-require("netrw") -- ./lua/netrw/init.lua
-require("feline-status") -- ./lua/feline-status/init.lua
-require("tree-keybindings") -- ./lua/tree-keybindings/init.lua
+require("plugins.undo-tree") -- ./lua/plugins/undo-tree/init.lua
+require("plugins.netrw") -- ./lua/plugins/netrw/init.lua
+require("plugins.feline-status") -- ./lua/plugins/feline-status/init.lua
+require("plugins.tree-keybindings") -- ./lua/plugins/tree-keybindings/init.lua
 require("vim-keybindings") -- ./lua/vim-keybindings/init.lua
 
 -- /// NAVIGATION ///.
 require("nv-telescope") -- ./lua/nv-telescope/init.lua
 require("telescope-keybindings") -- ./lua/telescope-keybindings.lua
-require("quick-scope") -- ./lua/quick-scope/init.lua
+require("plugins.quick-scope") -- ./lua/plugins/quick-scope/init.lua
 
 -- /// LS & SYNTAX ///.
-require("cmp-setup") -- ./lua/cmp-setup/init.lua
-require("treesitter") -- ./lua/treesitter/init.lua
-require("context-print") -- ./lua/context-print/init.lua
+require("plugins.cmp-setup") -- ./lua/plugins/cmp-setup/init.lua
+require("plugins.treesitter") -- ./lua/treesitter/init.lua
+require("plugins.context-print") -- ./lua/plugins/context-print/init.lua
 require("lsp") -- ./lua/lsp/
 require("lsp.lua") -- ./lua/lsp/lua.lua
 require("lsp.rust") -- ./lua/lsp/rust.lua
@@ -57,24 +57,24 @@ require("lsp.formatter") -- ./lua/lsp/formatter.lua
 require("lsp-trouble") -- ./lua/lsp-trouble/init.lua
 require("lsp-trouble-keybindings") -- ./lua/lsp-trouble-keybindings.lua
 require("symbols-outline") -- ./lua/symbols/init.lua
-require("nvim-commented") -- ./lua/nvim-commented/init.lua
+require("plugins.nvim-commented") -- ./lua/plugins/nvim-commented/init.lua
 
 -- /// OTHERS ///.
-require("web-devicons") -- ./lua/web-devicons/init.lua
-require("colorizer-setup") -- ./lua/colorizer-setup/init.lua
-require("lua-autopairs") -- (?) --./lua/lua-autopairs/init.lua
-require("markdown-preview") -- ./lua/markdown-preview/init.lua
-require("prime-harpoon") -- ./lua/prime-harpoon/init.lua
-require("autosession") -- ./lua/autosession/init.lua
-require("numb-nvim") -- ./lua/numb-nvim/init.lua
-require("vimtex") -- ./lua/vimtex/init.lua
-require("todo-finder") -- ./lua/todo-finder/init.lua
-require("trim-lua") -- ./lua/trim-lua/init.lua
-require("refactor") -- ./lua/refactor/init.lua
+require("plugins.web-devicons") -- ./lua/plugins/web-devicons/init.lua
+require("plugins.colorizer-setup") -- ./lua/plugins/colorizer-setup/init.lua
+-- require("lua-autopairs") -- (?) --./lua/lua-autopairs/init.lua
+require("plugins.markdown-preview") -- ./lua/plugins/markdown-preview/init.lua
+require("plugins.prime-harpoon") -- ./lua/plugins/prime-harpoon/init.lua
+require("plugins.autosession") -- ./lua/plugins/autosession/init.lua
+require("plugins.numb-nvim") -- ./lua/plugins/numb-nvim/init.lua
+require("plugins.vimtex") -- ./lua/plugins/vimtex/init.lua
+require("plugins.todo-finder") -- ./lua/plugins/todo-finder/init.lua
+require("plugins.trim-lua") -- ./lua/plugins/trim-lua/init.lua
+require("plugins.refactor") -- ./lua/plugins/refactor/init.lua
 
 -- /// Rust ///
-require("crates") -- ./lua/crates/init.lua
-require("rs-tools") -- ./lua/rs-tools/init.lua
+require("plugins.crates") -- ./lua/plugins/crates/init.lua
+require("plugins.rs-tools") -- ./lua/rs-tools/init.lua
 
 -- /// Git ///
 require("octo") -- ./lua/octo/init.lua
