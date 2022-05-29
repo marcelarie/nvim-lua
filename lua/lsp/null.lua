0820c507a759c2484492fa2dcda4be2f2a75b4f6
Marcel Arie <googlillo@gmail.com>
2022-05-29 20:41:08 +0200

    fix null.ls

--- lua/lsp/null.lua
+++ lua/lsp/null.lua
@@ -41,16 +41,17 @@ null_ls.setup {
 		-- formatting.prettierd,
 	},
 	on_attach = function(client, bufnr)
-		-- if client.supports_method "textDocument/formatting" then
-		-- 	vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-		-- 	vim.api.nvim_create_autocmd("BufWritePre", {
-		-- 		group = augroup,
-		-- 		buffer = bufnr,
-		-- 		callback = function()
-		-- 			lsp_formatting(bufnr)
-		-- 		end,
-		-- 	})
-		-- end
+		if client.supports_method "textDocument/formatting" then
+			vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- lsp_formatting(bufnr)
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
 	end,
 }
 
