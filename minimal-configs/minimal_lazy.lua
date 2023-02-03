a124b591761944a1770feb91f43d5b5116b37ec8
Marcel Arie <googlillo@gmail.com>
2023-02-03 14:39:54 +0100

    migrate all the plugins

--- minimal-configs/minimal_lazy.lua
+++ minimal-configs/minimal_lazy.lua
@@ -21,59 +21,312 @@ vim.opt.runtimepath:prepend(lazypath)
 
 -- install plugins
 local plugins = {
-		{ "lewis6991/impatient.nvim", rocks = "mpack" },
-		-- Colorschemes
-		"tjdevries/colorbuddy.nvim",
-		"lifepillar/vim-gruvbox8",
-		{ "mrjones2014/lighthaus.nvim" },
-		"phanviet/vim-monokai-pro",
-		"mhartington/oceanic-next",
-		"fenetikm/falcon",
-		"katawful/kat.nvim",
-		"rebelot/kanagawa.nvim",
-		{ "shaunsingh/oxocarbon.nvim", run = "./install.sh" },
-		{ "catppuccin/nvim", as = "catppuccin" },
-		{ "projekt0n/github-nvim-theme", tag = "v0.0.7" },
-		{
+	{ "lewis6991/impatient.nvim", rocks = "mpack" },
+	-- Colorschemes
+	"tjdevries/colorbuddy.nvim",
+	"lifepillar/vim-gruvbox8",
+	{ "mrjones2014/lighthaus.nvim" },
+	"phanviet/vim-monokai-pro",
+	"mhartington/oceanic-next",
+	"fenetikm/falcon",
+	"katawful/kat.nvim",
+	"rebelot/kanagawa.nvim",
+	-- { "shaunsingh/oxocarbon.nvim", build = "./install.sh" },
+	{ "catppuccin/nvim", name = "catppuccin" },
+	{ "projekt0n/github-nvim-theme", version = "v0.0.7" },
+	{
 
-			"jhchabran/monarized",
-			config = function()
-				vim.g.monarized_lualine = true
-				vim.g.monarized_kitty_colors = true
-			end,
-		},
-		"EdenEast/nightfox.nvim",
-		{ "shaeinst/roshnivim-cs" },
-		"npxbr/gruvbox.nvim",
-		"eddyekofo94/gruvbox-flat.nvim", -- <-- original,
-		"rktjmp/lush.nvim",
-		"savq/melange",
-		"ishan9299/modus-theme-vim",
-		"ishan9299/nvim-solarized-lua",
-		"aktersnurra/no-clown-fiesta.nvim",
+		"jhchabran/monarized",
+		config = function()
+			vim.g.monarized_lualine = true
+			vim.g.monarized_kitty_colors = true
+		end,
+	},
+	"EdenEast/nightfox.nvim",
+	{ "shaeinst/roshnivim-cs" },
+	"npxbr/gruvbox.nvim",
+	"eddyekofo94/gruvbox-flat.nvim", -- <-- original,
+	"rktjmp/lush.nvim",
+	"savq/melange",
+	"ishan9299/modus-theme-vim",
+	"ishan9299/nvim-solarized-lua",
+	"aktersnurra/no-clown-fiesta.nvim",
 
-		-- Telescope
-		"nvim-lua/popup.nvim",
-		"nvim-lua/plenary.nvim",
-		{
-			"nvim-telescope/telescope.nvim",
-			requires = {
-				{ "nvim-telescope/telescope-github.nvim" },
-			},
+	-- Telescope
+	"nvim-lua/popup.nvim",
+	"nvim-lua/plenary.nvim",
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{ "nvim-telescope/telescope-github.nvim" },
 		},
-		"nvim-telescope/telescope-media-files.nvim",
-		"nvim-telescope/telescope-fzy-native.nvim",
-		"nvim-telescope/telescope-symbols.nvim",
-		"dhruvmanila/telescope-bookmarks.nvim",
-		"nvim-telescope/telescope-github.nvim",
-		"nvim-telescope/telescope-packer.nvim",
-		"cljoly/telescope-repo.nvim",
-		"jvgrootveld/telescope-zoxide",
-		"ibhagwan/fzf-lua",
+	},
+	"nvim-telescope/telescope-media-files.nvim",
+	"nvim-telescope/telescope-fzy-native.nvim",
+	"nvim-telescope/telescope-symbols.nvim",
+	"dhruvmanila/telescope-bookmarks.nvim",
+	"nvim-telescope/telescope-github.nvim",
+	"nvim-telescope/telescope-packer.nvim",
+	"cljoly/telescope-repo.nvim",
+	"jvgrootveld/telescope-zoxide",
+	"ibhagwan/fzf-lua",
+	{ "kat0h/bufpreview.vim", build = "deno task prepare" },
+
+	-- LSP
+	"neovim/nvim-lsp",
+	"onsails/lspkind-nvim",
+	"neovim/nvim-lspconfig",
+	"barreiroleo/ltex-extra.nvim",
+	-- coq_nvim:,
+	-- main one,
+	{ "ms-jpq/coq_nvim", branch = "coq" },
+	-- 9000+ Snippets,
+	{ "ms-jpq/coq.artifacts", branch = "artifacts" },
+	-- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty,
+	-- Need to **configure separately**,
+	{ "ms-jpq/coq.thirdparty", branch = "3p" },
+	-- cmp,
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-buffer",
+		},
+	},
+	"hrsh7th/cmp-nvim-lsp",
+	"saadparwaiz1/cmp_luasnip",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/cmp-nvim-lua",
+	"andersevenrud/cmp-tmux",
+	-- { "github/copilot.vim" } -- needed for the auth
+	{
+		"zbirenbaum/copilot.lua",
+		event = "VimEnter",
+		config = function()
+			vim.defer_fn(function()
+				require("copilot").setup {
+					panel = {
+						enabled = true,
+						auto_refresh = false,
+						keymap = {
+							jump_prev = "[[",
+							jump_next = "]]",
+							accept = "<CR>",
+							refresh = "gr",
+							open = "<M-CR>",
+						},
+					},
+					suggestion = {
+						enabled = true,
+						auto_trigger = false,
+						debounce = 75,
+						keymap = {
+							accept = "<M-l>",
+							next = "<M-]>",
+							prev = "<M-[>",
+							dismiss = "<C-]>",
+						},
+					},
+					copilot_node_command = "node", -- Node version must be < 18
+					server_opts_overrides = {},
+				}
+			end, 100)
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		-- dependencies = { "copilot.lua", "nvim-cmp" },
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
+	{ "jose-elias-alvarez/null-ls.nvim" },
+	{ "MunifTanjim/eslint.nvim" },
+	"jose-elias-alvarez/nvim-lsp-ts-utils",
+	"simrat39/symbols-outline.nvim",
+	"jubnzv/virtual-types.nvim",
+	-- LSP Utils,
+	"RishabhRD/popfix",
+	"RishabhRD/nvim-lsputils",
+	-- Git
+	"tpope/vim-fugitive",
+	"lewis6991/gitsigns.nvim",
+	"f-person/git-blame.nvim",
+	"tpope/vim-abolish",
+	"sindrets/diffview.nvim",
+	"pwntester/octo.nvim",
+
+	-- TreeSitter
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	"nvim-treesitter/playground",
+	"JoosepAlviste/nvim-ts-context-commentstring",
+	"nvim-treesitter/nvim-treesitter-context",
+	{ "zsugabubus/crazy8.nvim" },
+	"eandrju/cellular-automaton.nvim",
+	"IndianBoy42/tree-sitter-just",
+	-- Nvim statusline,
+	"hoob3rt/lualine.nvim",
+	"rebelot/heirline.nvim",
+	-- Startup time,
+	"tweekmonster/startuptime.vim",
+	-- File explorers,
+	"kyazdani42/nvim-tree.lua",
+	"SidOfc/carbon.nvim",
+	--  Snippets,
+	"L3MON4D3/LuaSnip",
+	"honza/vim-snippets",
+	"thomasfaingnaert/vim-lsp-snippets",
+	"mlaursen/vim-react-snippets",
+	"dsznajder/vscode-es7-javascript-react-snippets",
+	"ChristianChiarulli/html-snippets",
+	"rafamadriz/friendly-snippets",
+	-- Latex,
+	"lervag/vimtex",
+	-- DAP
+	"mfussenegger/nvim-dap",
+	"vim-denops/denops.vim",
+	-- Rust
+	"simrat39/rust-tools.nvim",
+	"saecki/crates.nvim",
+
+	-- Others
+	"christoomey/vim-system-copy", -- System Copy
+	"windwp/nvim-ts-autotag", -- Autotags
+	"kylechui/nvim-surround",
+	"numToStr/Comment.nvim",
+	"mbbill/undotree",
+	"rafcamlet/nvim-luapad",
+	"kyazdani42/nvim-web-devicons",
+	"folke/lsp-trouble.nvim",
+	"tani/glance-vim",
+	"cappyzawa/trim.nvim",
+	"unblevable/quick-scope", -- An always-on highlight for a unique character in every word on a line to help you use f, F and family
+	"tversteeg/registers.nvim",
+	"folke/persistence.nvim",
+	"KabbAmine/vCoolor.vim", -- color picker
+	"nacro90/numb.nvim",
+	"folke/todo-comments.nvim", -- TODO finder
+	"fladson/vim-kitty", -- syntax highlight on kitty conf
+	"AckslD/nvim-neoclip.lua", -- clipboard manager with telescope
+	"tridactyl/vim-tridactyl",
+	{
+		"glacambre/firenvim",
+		build = function()
+			vim.fn["firenvim#install"](0)
+		end,
+	},
+	"arkav/lualine-lsp-progress",
+	"goolord/alpha-nvim",
+	"kosayoda/nvim-lightbulb",
+	{ "ray-x/guihua.lua", build = "cd lua/fzy && make" }, -- lua GUI lib
+	"ray-x/sad.nvim",
+	{
+		"ellisonleao/glow.nvim",
+		config = function()
+			require("glow").setup()
+		end,
+	},
+	{
+		"luukvbaal/stabilize.nvim",
+		config = function()
+			require("stabilize").setup()
+		end,
+	},
+	{ "tami5/sqlite.lua", --[[ module = "sqlite" ]] },
+	"lambdalisue/suda.vim",
+	"gbprod/substitute.nvim",
+	"max397574/better-escape.nvim",
+	{ "nyngwang/NeoRoot.lua" },
+	{
+		"jghauser/mkdir.nvim",
+		config = function()
+			require "mkdir"
+		end,
+	},
+	{
+		"klen/nvim-test",
+		config = function()
+			require("nvim-test").setup {}
+		end,
+	},
+	{ "farmergreg/vim-lastplace" },
+	"mickael-menu/zk-nvim",
+	{
+		"benfowler/telescope-luasnip.nvim",
+		-- module = "telescope._extensions.luasnip", -- if you wish to lazy-load
+	},
+	{ "akinsho/git-conflict.nvim", version = "v1.0.0" },
+	{
+		"johmsalas/text-case.nvim",
+		branch = "issue-16",
+		config = function()
+			require("textcase").setup {}
+			require("telescope").load_extension "textcase"
+			vim.api.nvim_set_keymap(
+				"n",
+				"ga.",
+				"TextCaseOpenTelescope",
+				{ desc = "Telescope" }
+			)
+			vim.api.nvim_set_keymap(
+				"v",
+				"ga.",
+				"TextCaseOpenTelescope",
+				{ desc = "Telescope" }
+			)
+		end,
+	},
+	{ "ZhiyuanLck/smart-pairs", event = "InsertEnter" },
+	"wellle/targets.vim",
+	{
+		"axkirillov/easypick.nvim",
+		-- dependencies = "nvim-telescope/telescope.nvim",
+	},
+	-- "vigoux/notifier.nvim"
+	"j-hui/fidget.nvim",
+	"folke/neodev.nvim",
+	{
+		"jbyuki/instant.nvim",
+		config = function()
+			vim.cmd 'let g:instant_username = "USERNAME"'
+		end,
+	},
+	{
+		"sQVe/sort.nvim",
+		config = function()
+			require("sort").setup {}
+		end,
+	},
+	{ "https://git.sr.ht/~whynothugo/lsp_lines.nvim" },
+
+	-- "ggandor/lightspeed.nvim"
+	"olimorris/persisted.nvim",
+	"smjonas/inc-rename.nvim",
+	"stevearc/dressing.nvim",
+	{
+		"AckslD/nvim-FeMaco.lua",
+		config = function()
+			require("femaco").setup()
+		end,
+	},
+	"brenoprata10/nvim-highlight-colors",
+	"AckslD/nvim-trevJ.lua",
+	"ggandor/lightspeed.nvim",
+	"ofirgall/open.nvim",
+	{
+		"declancm/cinnamon.nvim",
+		config = function()
+			require("cinnamon").setup()
+		end,
+	},
+	{
+		"tjdevries/sg.nvim",
+		build = "cargo build --workspace",
+	},
 }
 
 require("lazy").setup(plugins, {
 	root = root .. "/plugins",
 })
 
-vim.cmd("colorscheme catppuccin")
+vim.cmd "colorscheme catppuccin"
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -177,7 +177,7 @@ _G.packer_plugins = {
   },
   ["copilot.lua"] = {
     after = { "copilot-cmp" },
-    config = { "\27LJ\2\nß\2\0\0\5\0\f\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\6\0005\3\3\0005\4\4\0=\4\5\3=\3\a\0025\3\b\0005\4\t\0=\4\5\3=\3\n\0024\3\0\0=\3\v\2B\0\2\1K\0\1\0\26server_opts_overrides\15suggestion\1\0\4\tnext\n<M-]>\fdismiss\n<C-]>\tprev\n<M-[>\vaccept\n<M-l>\1\0\3\rdebounce\3K\fenabled\2\17auto_trigger\1\npanel\1\0\1\25copilot_node_command\tnode\vkeymap\1\0\5\topen\v<M-CR>\14jump_next\a]]\vaccept\t<CR>\14jump_prev\a[[\frefresh\agr\1\0\2\17auto_refresh\1\fenabled\2\nsetup\fcopilot\frequire-\1\0\4\0\3\0\0066\0\0\0009\0\1\0003\2\2\0)\3d\0B\0\3\1K\0\1\0\0\rdefer_fn\bvim\0" },
+    config = { "\27LJ\2\nß\2\0\0\5\0\f\0\0176\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\6\0005\3\3\0005\4\4\0=\4\5\3=\3\a\0025\3\b\0005\4\t\0=\4\5\3=\3\n\0024\3\0\0=\3\v\2B\0\2\1K\0\1\0\26server_opts_overrides\15suggestion\1\0\4\tprev\n<M-[>\vaccept\n<M-l>\tnext\n<M-]>\fdismiss\n<C-]>\1\0\3\17auto_trigger\1\rdebounce\3K\fenabled\2\npanel\1\0\1\25copilot_node_command\tnode\vkeymap\1\0\5\frefresh\agr\topen\v<M-CR>\14jump_next\a]]\14jump_prev\a[[\vaccept\t<CR>\1\0\2\17auto_refresh\1\fenabled\2\nsetup\fcopilot\frequire-\1\0\4\0\3\0\0066\0\0\0009\0\1\0003\2\2\0)\3d\0B\0\3\1K\0\1\0\0\rdefer_fn\bvim\0" },
     loaded = false,
     needs_bufread = false,
     only_cond = false,
@@ -854,6 +854,14 @@ if not vim.g.packer_custom_loader_enabled then
   vim.g.packer_custom_loader_enabled = true
 end
 
+-- Config for: sort.nvim
+time([[Config for sort.nvim]], true)
+try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\tsort\frequire\0", "config", "sort.nvim")
+time([[Config for sort.nvim]], false)
+-- Config for: nvim-FeMaco.lua
+time([[Config for nvim-FeMaco.lua]], true)
+try_loadstring("\27LJ\2\n4\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\vfemaco\frequire\0", "config", "nvim-FeMaco.lua")
+time([[Config for nvim-FeMaco.lua]], false)
 -- Config for: nvim-test
 time([[Config for nvim-test]], true)
 try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-test\frequire\0", "config", "nvim-test")
@@ -862,38 +870,30 @@ time([[Config for nvim-test]], false)
 time([[Config for monarized]], true)
 try_loadstring("\27LJ\2\nZ\0\0\2\0\4\0\t6\0\0\0009\0\1\0+\1\2\0=\1\2\0006\0\0\0009\0\1\0+\1\2\0=\1\3\0K\0\1\0\27monarized_kitty_colors\22monarized_lualine\6g\bvim\0", "config", "monarized")
 time([[Config for monarized]], false)
--- Config for: sort.nvim
-time([[Config for sort.nvim]], true)
-try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\tsort\frequire\0", "config", "sort.nvim")
-time([[Config for sort.nvim]], false)
--- Config for: instant.nvim
-time([[Config for instant.nvim]], true)
-try_loadstring("\27LJ\2\nG\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0(let g:instant_username = \"USERNAME\"\bcmd\bvim\0", "config", "instant.nvim")
-time([[Config for instant.nvim]], false)
--- Config for: text-case.nvim
-time([[Config for text-case.nvim]], true)
-try_loadstring("\27LJ\2\n…\2\0\0\6\0\14\0\0296\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\0016\0\0\0'\2\3\0B\0\2\0029\0\4\0'\2\1\0B\0\2\0016\0\5\0009\0\6\0009\0\a\0'\2\b\0'\3\t\0'\4\n\0005\5\v\0B\0\5\0016\0\5\0009\0\6\0009\0\a\0'\2\f\0'\3\t\0'\4\n\0005\5\r\0B\0\5\1K\0\1\0\1\0\1\tdesc\14Telescope\6v\1\0\1\tdesc\14Telescope\26TextCaseOpenTelescope\bga.\6n\20nvim_set_keymap\bapi\bvim\19load_extension\14telescope\nsetup\rtextcase\frequire\0", "config", "text-case.nvim")
-time([[Config for text-case.nvim]], false)
--- Config for: stabilize.nvim
-time([[Config for stabilize.nvim]], true)
-try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14stabilize\frequire\0", "config", "stabilize.nvim")
-time([[Config for stabilize.nvim]], false)
--- Config for: mkdir.nvim
-time([[Config for mkdir.nvim]], true)
-try_loadstring("\27LJ\2\n%\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\nmkdir\frequire\0", "config", "mkdir.nvim")
-time([[Config for mkdir.nvim]], false)
 -- Config for: cinnamon.nvim
 time([[Config for cinnamon.nvim]], true)
 try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rcinnamon\frequire\0", "config", "cinnamon.nvim")
 time([[Config for cinnamon.nvim]], false)
+-- Config for: stabilize.nvim
+time([[Config for stabilize.nvim]], true)
+try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14stabilize\frequire\0", "config", "stabilize.nvim")
+time([[Config for stabilize.nvim]], false)
+-- Config for: text-case.nvim
+time([[Config for text-case.nvim]], true)
+try_loadstring("\27LJ\2\n…\2\0\0\6\0\14\0\0296\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\0016\0\0\0'\2\3\0B\0\2\0029\0\4\0'\2\1\0B\0\2\0016\0\5\0009\0\6\0009\0\a\0'\2\b\0'\3\t\0'\4\n\0005\5\v\0B\0\5\0016\0\5\0009\0\6\0009\0\a\0'\2\f\0'\3\t\0'\4\n\0005\5\r\0B\0\5\1K\0\1\0\1\0\1\tdesc\14Telescope\6v\1\0\1\tdesc\14Telescope\26TextCaseOpenTelescope\bga.\6n\20nvim_set_keymap\bapi\bvim\19load_extension\14telescope\nsetup\rtextcase\frequire\0", "config", "text-case.nvim")
+time([[Config for text-case.nvim]], false)
 -- Config for: glow.nvim
 time([[Config for glow.nvim]], true)
 try_loadstring("\27LJ\2\n2\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\tglow\frequire\0", "config", "glow.nvim")
 time([[Config for glow.nvim]], false)
--- Config for: nvim-FeMaco.lua
-time([[Config for nvim-FeMaco.lua]], true)
-try_loadstring("\27LJ\2\n4\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\vfemaco\frequire\0", "config", "nvim-FeMaco.lua")
-time([[Config for nvim-FeMaco.lua]], false)
+-- Config for: mkdir.nvim
+time([[Config for mkdir.nvim]], true)
+try_loadstring("\27LJ\2\n%\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\nmkdir\frequire\0", "config", "mkdir.nvim")
+time([[Config for mkdir.nvim]], false)
+-- Config for: instant.nvim
+time([[Config for instant.nvim]], true)
+try_loadstring("\27LJ\2\nG\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0(let g:instant_username = \"USERNAME\"\bcmd\bvim\0", "config", "instant.nvim")
+time([[Config for instant.nvim]], false)
 -- Load plugins in order defined by `after`
 time([[Sequenced loading]], true)
 vim.cmd [[ packadd nvim-cmp ]]
