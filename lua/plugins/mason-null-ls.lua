59d5031616c5f82c88e18bafa5d301ef7e6de78a
marcelarie <googlillo@gmail.com>
2024-01-22 15:26:45 +0100

    formatting with stylua and fixjson

---
+++ lua/plugins/mason-null-ls.lua
@@ -0,0 +1,73 @@
+return {
+	"jay-babu/mason-null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls.nvim",
+	},
+	config = function()
+		local null_ls = require "null-ls"
+		local formatting = null_ls.builtins.formatting
+		local diagnostics = null_ls.builtins.diagnostics
+		-- local completion = null_ls.builtins.completion
+		local code_actions = null_ls.builtins.code_actions
+
+		null_ls.setup {
+			sources = {
+				formatting.stylua,
+				formatting.shfmt,
+				formatting.fixjson,
+				formatting.fish_indent,
+				formatting.mdformat,
+				formatting.prettierd,
+				formatting.ocamlformat,
+
+				-- formatting.rubocop,
+				-- diagnostics.rubocop,
+				diagnostics.shellcheck,
+				-- diagnostics.eslint_d.with { condition = eslint_condition },
+				-- formatting.eslint_d.with { condition = eslint_condition },
+				-- code_actions.eslint_d.with { condition = eslint_condition },
+				code_actions.gitsigns,
+				code_actions.shellcheck,
+				code_actions.gitrebase,
+				code_actions.refactoring,
+				-- code_actions.ltrs, -- LanguageTool API
+
+				-- formatting.deno_fmt.with { condition = deno_condition },
+				-- diagnostics.gitlint,
+				-- diagnostics.selene,
+				-- diagnostics.commitlint,
+				-- diagnostics.luacheck,
+				diagnostics.fish,
+				-- diagnostics.ltrs, -- LanguageTool API
+
+				-- PYTHON
+				formatting.ruff,
+				diagnostics.ruff,
+				-- formatting.black,
+				-- diagnostics.flake8,
+
+				formatting.taplo,
+
+				formatting.fixjson,
+
+				formatting.perltidy,
+				formatting.gofumpt,
+			},
+		}
+
+		require("mason-null-ls").setup {
+			ensure_installed = { "stylua", "jq" },
+			handlers = {
+				stylua = function(source_name, methods)
+					null_ls.register(null_ls.builtins.formatting.stylua)
+				end,
+				shfmt = function(source_name, methods)
+					-- custom logic
+					require("mason-null-ls").default_setup(source_name, methods) -- to maintain default behavior
+				end,
+			},
+		}
+	end,
+}
