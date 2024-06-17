285980693c929653b1923375a4ec26eac570ddf3
marcelarie <googlillo@gmail.com>
2024-06-17 16:08:17 +0200

    update

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -164,18 +164,18 @@ local servers = {
 			"deno.jsonc"
 		),
 	},
-	llm_ls = {
-		model = "codellama:7b",
-		url = "http://localhost:11434", -- llm-ls uses "/api/generate"
-		-- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
-		request_body = {
-			-- Modelfile options for the model you use
-			options = {
-				temperature = 0.2,
-				top_p = 0.95,
-			},
-		},
-	},
+	-- llm_ls = {
+	-- 	model = "codellama:7b",
+	-- 	url = "http://localhost:11434", -- llm-ls uses "/api/generate"
+	-- 	-- cf https://github.com/ollama/ollama/blob/main/docs/api.md#parameters
+	-- 	request_body = {
+	-- 		-- Modelfile options for the model you use
+	-- 		options = {
+	-- 			temperature = 0.2,
+	-- 			top_p = 0.95,
+	-- 		},
+	-- 	},
+	-- },
 
 	lua_ls = {
 		settings = {
