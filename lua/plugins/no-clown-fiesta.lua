4e370a3606717c2ddbcb079e19fd037abc3f022c
marcelarie <googlillo@gmail.com>
2024-10-28 14:42:51 +0100

    update

--- lua/plugins/no-clown-fiesta.lua
+++ lua/plugins/no-clown-fiesta.lua
@@ -1,4 +1,6 @@
+-- local fork_location = "~/clones/forks/no-clown-fiesta.nvim"
+
 return {
 	"aktersnurra/no-clown-fiesta.nvim",
-	dir = "~/clones/forks/no-clown-fiesta.nvim",
+	-- dir = fork_location
 }
